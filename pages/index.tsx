ad77b4dcf3bd3ec6653babad007d3344a15b2e46
peculiarnewbie <curiousarmand@gmail.com>
2022-06-07 19:57:23 +0700

    link to previous porto

--- pages/index.tsx
+++ pages/index.tsx
@@ -128,12 +128,11 @@ const Home: NextPage<Props> = (props) => {
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://peculiarnewbie.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
