cd11db1b54c793e8d315060eec3da5c017cafb35
peculiarnewbie <curiousarmand@gmail.com>
2022-06-20 14:11:42 +0700

    Projects very cool wave bg

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,8 +12,8 @@ function Projects({header, children}:Params){
 
     return(
         <div className="w-full relative pb-8">
-            <div className="absolute h-full -left-[10%] w-[120%] -z-10 bg-slate-200 shadow-up">
-            </div>
+            <div className="absolute h-full -left-[100%] top-[550px] lg:top-[650px] w-[300%] -z-[11] bg-slate-200 shadow-up"></div>
+            
             <div className="absolute w-1/2 h-10 -left-1/2 z-20  "></div>
             <p className="font-bold text-4xl m-4">{header}</p>
             <div className="flex flex-1 flex-wrap ">
--- components/TopContent.tsx
+++ components/TopContent.tsx
@@ -1,9 +1,10 @@
 function TopContent(){
     return(
         <div id="top" className="flex-col w-[120%] h-[80vh] px-[10%] ">
-            <div className="absolute left-10 top-0 w-[110%] h-full -z-[10]">
+            <div className="absolute left-10 top-0 w-[110%] h-full -z-[15]">
                 <img className="fixed h-full w-full object-cover" src="/img/mesh.png"></img>
             </div>
+            
             <div className="flex items-end h-2/3 sm:h-3/5">
                 <div className="relative bottom-0 w-full items-center">
                     <h1 className="font-semibold text-7xl md:text-9xl md:text-center">Hi!</h1>
@@ -12,6 +13,11 @@ function TopContent(){
                         <p className="py-2 md:py-6 md:text-2xl md:text-center w-3/5">I create games, develop front-end, and animate... animations</p>
                     </div>
                     
+                </div>
+            </div>
+            <div className="flex absolute left-10 top-[80vh] w-[120%] h-[1080px] -z-[10]">
+                <div className="flex relative w-full h-full grow">
+                    <img className="absolute -top-[700px] lg:-top-[600px] h-[1280px] object-cover grow" src="/img/Wave.svg"></img>
                 </div>
             </div>
         </div>
