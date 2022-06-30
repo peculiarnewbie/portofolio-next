89bf1eeaa7c90a71db44e2ace26b79c61cc7a21f
peculiarnewbie <curiousarmand@gmail.com>
2022-06-30 14:27:39 +0700

    logos

--- components/TopContent.tsx
+++ components/TopContent.tsx
@@ -1,4 +1,19 @@
+import { Html } from "next/document";
+
 function TopContent(){
+    function ToggleDark(){
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+          } else {
+            document.documentElement.classList.remove('dark')
+          }
+          
+          if(localStorage.theme == 'light') localStorage.theme = 'dark'
+          else localStorage.theme = 'light'
+          
+          // Whenever the user explicitly chooses dark mode
+    }
+
     return(
         <div id="top" className="flex-col w-[120%] h-[80vh] px-[10%] ">
             <div className="absolute left-10 top-0 w-[110%] h-full -z-[15]">
@@ -8,10 +23,31 @@ function TopContent(){
             
             <div className="flex items-end h-2/3 sm:h-3/5">
                 <div className="relative bottom-0 w-full items-center dark:text-white">
-                    <h1 className="font-semibold text-7xl md:text-9xl md:text-center">Hi!</h1>
+                    <h1 onClick={ToggleDark} className="font-semibold text-7xl md:text-9xl md:text-center">Hi!</h1>
                     <h2 className="md:pt-2 font-semibold text-3xl md:text-5xl md:text-center">My name is Arif</h2>
                     <div className="flex w-full md:justify-center">
                         <p className="py-2 md:py-6 md:text-2xl md:text-center w-3/5">I create games, develop front-end, and animate... animations</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className="flex w-full h-8 justify-center">
+                <div className="flex h-full w-fit justify-center gap-12 dark:invert">
+                    <div className="flex-col h-full">
+                        <img className="h-full" src="/logos/mail.svg"/>
+                        <p className="text-center font-medium">github</p>
+                    </div>
+                    <div className="flex-col h-full items-center">
+                        <img className="h-full" src="/logos/github.svg"/>
+                        <p className="text-center font-medium">github</p>
+                    </div>
+                    <div className="flex-col h-full">
+                        <img className="h-full" src="/logos/itchio.svg"/>
+                        <p className="text-center font-medium">itch.io</p>
+                    </div>
+                    <div className="flex-col h-full align-center">
+                        <img className="h-full grayscale items-center brightness-0" src="/logos/artstation.svg"/>
+                        <p className="text-center font-medium">artstation</p>
                     </div>
                     
                 </div>
